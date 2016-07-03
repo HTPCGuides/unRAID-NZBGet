634d664f70dd379266b97e260b2781214eda3b1e
Desgyz <audibailey7@gmail.com>
2016-07-03 11:50:14 +1000

    Fixed Permission Errors

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+usermod -u 99 nobody
+usermod -g 100 nobody
+usermod -d /home nobody
+chown -R nobody:users /home
 
 if [[ ! -f /config/nzbget.conf ]]; then 
 	cp /opt/nzbget/nzbget.conf /config/nzbget.conf
@@ -7,5 +11,9 @@ fi
 if [[ ! -f /config/nzbget.log ]]; then
 	touch /config/nzbget.log
 fi
+
+chown -R nobody:users /config /opt/nzbget
+
+tail -f /config/nzbget.log
 
 /opt/nzbget/nzbget -c /config/nzbget.conf -D
