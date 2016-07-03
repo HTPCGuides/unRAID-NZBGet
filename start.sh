16b74a4f7ce241e3ef5fbc83c3b8d55def5d886f
Desgyz <audibailey7@gmail.com>
2016-07-03 15:14:55 +1000

    Fixing Config

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ chown -R nobody:users /home
 
 if [[ ! -f /config/nzbget.conf ]]; then 
 	cp /opt/nzbget/nzbget.conf /config/nzbget.conf
+	sed -i -e "s#\(WebDir=\).*#\1/opt/nzbget/webui#g" /config/nzbget.conf
+	sed -i -e 's#\(ConfigTemplate=\).*#\1/opt/nzbget/webui/nzbget.conf.template#g' /config/nzbget.conf
+	sed -i -e "s#\(LogFile=\).*#\1/config/nzbget.log#g" /config/nzbget.conf
 fi
 
 if [[ ! -f /config/nzbget.log ]]; then
