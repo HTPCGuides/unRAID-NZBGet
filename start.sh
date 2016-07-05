f599211f44d3dc39f4b99164cfc1b4c9fe6c5b4e
Desgyz <audibailey7@gmail.com>
2016-07-05 15:33:12 +1000

    Removed Uneeded commands and attempted to fix the no launch error

--- start.sh
+++ start.sh
@@ -6,9 +6,6 @@ chown -R nobody:users /home
 
 if [[ ! -f /config/nzbget.conf ]]; then 
 	cp /opt/nzbget/nzbget.conf /config/nzbget.conf
-	sed -i -e "s#\(WebDir=\).*#\1/opt/nzbget/webui#g" /config/nzbget.conf
-	sed -i -e 's#\(ConfigTemplate=\).*#\1/opt/nzbget/webui/nzbget.conf.template#g' /config/nzbget.conf
-	sed -i -e "s#\(LogFile=\).*#\1/config/nzbget.log#g" /config/nzbget.conf
 fi
 
 if [[ ! -f /config/nzbget.log ]]; then
@@ -19,4 +16,4 @@ chown -R nobody:users /config /opt/nzbget
 
 tail -f /config/nzbget.log
 
-sh /sbin/setuser nobody /opt/nzbget/nzbget -D -c /config/nzbget.conf
+./opt/nzbget/nzbget -D -c /config/nzbget.conf
